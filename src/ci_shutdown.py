87e6ca55b1b8524705798252669536747a5b9dca
BlumenKopf <72992408+BlumenKopf@users.noreply.github.com>
2021-04-15 14:26:27 +0200

    Update ci_shutdown.py

--- src/ci_shutdown.py
+++ src/ci_shutdown.py
@@ -8,11 +8,11 @@ def main():
     while True: 
         userin = input('You want Shutdown (s) or Restart (r) your System? (s/r)')
         if userin in ['R', 'r', 'S', 's']: break
-    path = r"D:\_Projekte"
+    work_path = 'D:/SVN/V-Projekte'
     today = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M')) 
     msg = f'Auto Commit {today}'
-    check_for_add(path)
-    os.system(f'svn ci {path} -m "{msg}"')
+    check_for_add(work_path)
+    os.system(f'svn ci "{work_path}" -m "{msg}"')
     print('Succsesfull commited')
     time.sleep(5)    
     if userin in ['s', 'S']:
@@ -20,28 +20,11 @@ def main():
     elif userin in ['r', 'R']:
         os.system('shutdown.exe /r /t 00')   
 
-def check_for_add(path):
-    status = subprocess.run(f'svn status "{path}"', stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+def check_for_add(work_path):
 
-    for status in status.stdout.splitlines():
-        if status[0] == '?':
-            sts = status.replace('?', '').strip()
-            os.system(f'svn add "{sts}"')
-    pass
-
-def check_for_revert(path):
-    
-    status = subprocess.run(f'svn status "{path}"', stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-
-    for status in status.stdout.splitlines():
-        if status[0] == '!':
-            sts = status.replace('!', '').strip()
-            #while True:
-                #revert = input(f'You want revert {sts} (Y/N)')
-                #if revert in ['N', 'n', 'Y', 'y']: break
-            #if revert in ['N', 'n']: return
-            os.system(f'svn revert "{sts}"')
-            time.sleep(2)
+    for work_copy in os.listdir(work_path):
+        work_copy = work_path + '/' + work_copy
+        os.system(f'svn add "{work_copy}"')
 
 if __name__ == '__main__':
     main()
