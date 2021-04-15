08adf04fb7e625df11505c37bb4d1cc34b3d9c4d
BlumenKopf <72992408+BlumenKopf@users.noreply.github.com>
2021-04-15 16:40:31 +0200

    Update ci_shutdown.py

--- src/ci_shutdown.py
+++ src/ci_shutdown.py
@@ -9,22 +9,27 @@ def main():
         userin = input('You want Shutdown (s) or Restart (r) your System? (s/r)')
         if userin in ['R', 'r', 'S', 's']: break
     work_path = 'D:/SVN/V-Projekte'
+    
     today = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M')) 
     msg = f'Auto Commit {today}'
-    check_for_add(work_path)
-    os.system(f'svn ci "{work_path}" -m "{msg}"')
-    print('Succsesfull commited')
+
+    for work_copy in os.listdir(work_path):
+        work_copy = work_path + '/' + work_copy
+        check_for_add(work_copy)
+        check_commit(work_copy, msg)
+    
     time.sleep(5)    
     if userin in ['s', 'S']:
         os.system('shutdown.exe /s /t 00')
     elif userin in ['r', 'R']:
         os.system('shutdown.exe /r /t 00')   
 
-def check_for_add(work_path):
+def check_for_add(work_copy):
+    os.system(f'svn add "{work_copy}" --force')
 
-    for work_copy in os.listdir(work_path):
-        work_copy = work_path + '/' + work_copy
-        os.system(f'svn add "{work_copy}"')
+def check_commit(work_copy, msg):
+    os.system(f'svn ci "{work_copy}" -m "{msg}"')
+    print('Succsesfull commited')
 
 if __name__ == '__main__':
     main()
