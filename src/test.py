3579c8a74ba6c1978f00fbbad0664f7e59090c96
ChrisBran0815 <72992408+ChrisBran0815@users.noreply.github.com>
2021-07-22 10:35:26 +0200

    Update test.py

--- src/test.py
+++ src/test.py
@@ -4,9 +4,13 @@ import subprocess
 
 def main():
     path = '/home/christoph/Dokumente/test/trunk'
-    cmd = " ".join(['svn info --show-item=url', path])
-    status = subprocess.Popen(cmd, shell=True)
+    path2 = r'D:\SVN\V-Projekte\VT_20210712-05_Hawle_Armaturenwerke_GmbH'
+    cmd = " ".join(['svn info --show-item=url', path2])
+    status = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 
+    for i in status.stdout:
+        print(i)
+    
 
 if __name__ == '__main__':
     main()
