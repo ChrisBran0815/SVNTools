e69bdb502680b0b2f966da5ed9b30462431b70f5
Christoph Brandau <c.brandau91@gmail.com>
2021-08-11 21:06:28 +0200

    - add something

--- src/test.py
+++ src/test.py
@@ -1,12 +1,26 @@
-from os import popen
+import os
 import subprocess
 
 
 def main():
     path = '/home/christoph/Dokumente/test/trunk'
     cmd = " ".join(['svn info --show-item=url', path])
-    status = subprocess.Popen(cmd, shell=True)
+    status = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE ,universal_newlines=True)
 
-
+    print(status.stdout.splitlines())
+    i = 0
+    while i == 0:
+        print('HELLO')
+        cmd = " ".join(['svn status', path])
+        list = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE ,universal_newlines=True)
+        print(list.stdout.splitlines())
+        for content in list.stdout.splitlines():
+            if "!" in content:
+                print('yes')
+                cmd=' '.join(['svn rm', content[1:]])
+                test = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE ,universal_newlines=True)
+                print(test.stdout.splitlines())
+        i += 1
+    
 if __name__ == '__main__':
     main()
