0116f6885048ece9c0153dd5089a9f02c235b0df
ChrisBran0815 <72992408+ChrisBran0815@users.noreply.github.com>
2021-08-19 13:37:18 +0200

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,13 +1,30 @@
 import os
 import subprocess
+import platform
 
 
 def main():
-    path = '/home/christoph/Dokumente/test/trunk'
-    cmd = " ".join(['svn info --show-item=url', path])
-    status = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE ,universal_newlines=True)
+    if platform.system() == 'Linux':
+        path = '/home/christoph/Dokumente/test/trunk'
+    elif platform.system() == 'Windows':
+        path =r'D:\SVN\V-Projekte'
 
-    print(status.stdout.splitlines())
+    for wk in os.listdir(path):
+        wk = os.path.join(path, f'"{wk}"')
+        cmd = " ".join(['svn st', wk])
+        output = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        for status in output.stdout.splitlines():
+            if status[:1] == '!':
+                cmd = ' '.join(['svn rm', f'"{status[8:]}"'])
+                print(subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True))
+                print(status[8:])
+            #status = status[1:]
+            
+            print(status)
+
+   # !M      D:\SVN\V-Projekte\VT_20210816-13_Warema\02_CAM\01_Setup\519378_ke031610051_165-setup_1  Copy.prt    
+   # A       D:\SVN\V-Projekte\VT_20210816-13_Warema\02_CAM\01_Setup\519378_ke031610051_165-setup_1.prt
+    return
     i = 0
     while i == 0:
         print('HELLO')
@@ -24,3 +41,4 @@ def main():
     
 if __name__ == '__main__':
     main()
+    
