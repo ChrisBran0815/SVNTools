f7b7a413736f8313c68da0270cc5e4ec7354218e
ChrisBran0815 <72992408+ChrisBran0815@users.noreply.github.com>
2021-08-20 15:12:57 +0200

    Update ci_shutdown.py

--- src/ci_shutdown.py
+++ src/ci_shutdown.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import platform
+import time
 
 def add(item):
     cmd = ' '.join([f'svn add "{item}" --force'])
@@ -8,7 +9,9 @@ def add(item):
 
 def commit(item, msg):
     cmd = ' '.join([f'svn ci {item} -m "{msg}"'])
-    subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    ci = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True, stderr=subprocess.PIPE, universal_newlines=True)
+    for info in ci.stdout.splitlines():
+        print(info)
 
 def remove(item):    
     cmd = ' '.join(f'svn rm {item}')
@@ -36,12 +39,23 @@ def main():
                 continue
             elif status[:1] == 'D':
                 continue   
+            elif status[:1] == 'M':
+                continue
             else:
                 print(status)
-    msg = f'--> auto commit Proj. {os.path.basename(wc)}'
-    commit(item=wc, msg=msg)
+        msg = f'--> auto commit Proj. {os.path.basename(wc)}'
+        commit(item=wc, msg=msg)
     return
    
 if __name__ == '__main__':
+    while True:
+        user_input = input('You want Shutdown (s) or Restart (r) your System? (s/r)')
+        if user_input in ['R', 'r', 'S', 's']: break
+
     main()
+    time.sleep(5)
+    if user_input in ['s', 'S']:
+        os.system('shutdown.exe /s /t 00')
+    elif user_input in ['r', 'R']:
+        os.system('shutdown.exe /r /t 00')
     
