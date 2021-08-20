3a72b7e67531926c2d8d12c05f20b35c13de972f
ChrisBran0815 <72992408+ChrisBran0815@users.noreply.github.com>
2021-08-20 15:03:53 +0200

    Update ci_shutdown.py

--- src/ci_shutdown.py
+++ src/ci_shutdown.py
@@ -1,35 +1,47 @@
-#!/usr/bin/env python3.8
 import os
-import datetime
-import time
 import subprocess
+import platform
+
+def add(item):
+    cmd = ' '.join([f'svn add "{item}" --force'])
+    subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+
+def commit(item, msg):
+    cmd = ' '.join([f'svn ci {item} -m "{msg}"'])
+    subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+
+def remove(item):    
+    cmd = ' '.join(f'svn rm {item}')
+    subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)    
 
 def main():
-    while True: 
-        userin = input('You want Shutdown (s) or Restart (r) your System? (s/r)')
-        if userin in ['R', 'r', 'S', 's']: break
-    work_path = 'D:/SVN/V-Projekte'
-    
-    today = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M')) 
-    msg = f'Auto Commit {today}'
 
-    for work_copy in os.listdir(work_path):
-        work_copy = work_path + '/' + work_copy
-        check_for_add(work_copy)
-        check_commit(work_copy, msg)
-    
-    time.sleep(5)    
-    if userin in ['s', 'S']:
-        os.system('shutdown.exe /s /t 00')
-    elif userin in ['r', 'R']:
-        os.system('shutdown.exe /r /t 00')   
+    if platform.system() == 'Linux':
+        path = '/home/christoph/Dokumente/test/trunk'
+    elif platform.system() == 'Windows':
+        path =r'D:\SVN\V-Projekte'
 
-def check_for_add(work_copy):
-    os.system(f'svn add "{work_copy}" --force')
-
-def check_commit(work_copy, msg):
-    os.system(f'svn ci "{work_copy}" -m "{msg}"')
-    print('Succsesfull commited')
-
+    for wc in os.listdir(path):
+        wc = os.path.join(path, wc)
+        cmd = " ".join(['svn st', wc])
+        output = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        for status in output.stdout.splitlines():            
+            if status[:1] == '!':
+                remove(item=status[8:])
+                continue
+            elif status[:1] == 'A':
+                continue
+            elif status[:1] == '?':
+                add(status[8:])
+                continue
+            elif status[:1] == 'D':
+                continue   
+            else:
+                print(status)
+    msg = f'--> auto commit Proj. {os.path.basename(wc)}'
+    commit(item=wc, msg=msg)
+    return
+   
 if __name__ == '__main__':
     main()
+    
